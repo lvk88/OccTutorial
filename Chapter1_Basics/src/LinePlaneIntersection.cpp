6e5b7e9601afbfc75f71adaa7a69e0f29292f356
lvk88 <laszlokudela@gmail.com>
2015-11-19 20:14:14 +0100

    Changed to a better algorithm from math_DirectPolynomialRoots

--- Chapter1_Basics/src/LinePlaneIntersection.cpp
+++ Chapter1_Basics/src/LinePlaneIntersection.cpp
@@ -6,6 +6,7 @@
 
 #include "math_FunctionWithDerivative.hxx"
 #include "math_FunctionRoot.hxx"
+#include "math_DirectPolynomialRoots.hxx"
 #include "ElCLib.hxx"
 
 class LinePlaneIntersectionFunction: public math_FunctionWithDerivative {
@@ -71,13 +72,33 @@ LinePlaneIntersection::~LinePlaneIntersection() {
 gp_Pnt LinePlaneIntersection::intersectLineWithPlane(const gp_Lin& line,
 		const gp_Pln& plane) {
 
-	LinePlaneIntersectionFunction functionToSolve(line,plane);
+//	LinePlaneIntersectionFunction functionToSolve(line,plane);
+//
+//	Standard_Real firstGuess = 10.0;
+//	math_FunctionRoot rootFinder(functionToSolve,firstGuess,1E-10);
+//
+//	Standard_Real root = rootFinder.Root();
+//
+//	gp_Pnt result = ElCLib::Value(root,line);
 
-	Standard_Real firstGuess = 10.0;
-	math_FunctionRoot rootFinder(functionToSolve,firstGuess,1E-10);
+	//Gather the coefficients of the plane
+	//Ax+By*Cz*D=0
+	Standard_Real A,B,C,D;
+	plane.Coefficients(A,B,C,D);
 
-	Standard_Real root = rootFinder.Root();
+	//Set up equation Ax(u)+B*y(u)+C*z(u)+D=0
+	//A(x0+dx*u)+B*(y0+dy*u)+C(z0+dz*u)+D=0
+	//u(A*dx+B*dy+C*dz)+(A*x0+B*y0+C*z0+D)=0
+	//u*X+Y=0
+	gp_Pnt lineBasePoint = line.Location();
+	gp_Dir lineDirection = line.Direction();
 
+	Standard_Real X = A*lineDirection.X()+B*lineDirection.Y()+C*lineDirection.Z();
+	Standard_Real Y = A*lineBasePoint.X()+lineBasePoint.Y()+lineBasePoint.Z()+D;
+
+	math_DirectPolynomialRoots rootFinder(X,Y);
+
+	Standard_Real root = rootFinder.Value(1);
 	gp_Pnt result = ElCLib::Value(root,line);
 
 	return result;
